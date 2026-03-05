b35b8b87eb5da41272620f344af60a43210357d6
nmollers <210981@vistacollege.nl>
2026-03-05 10:20:09 +0100

    Tabs op de bewijspagina aangepast

---
+++ js/tabs.js
@@ -0,0 +1,15 @@
+const tabs = document.querySelectorAll(".tab");
+const contents = document.querySelectorAll(".tab-content");
+
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    tabs.forEach((t) => t.classList.remove("active"));
+    contents.forEach((c) => c.classList.remove("active"));
+
+    tab.classList.add("active");
+
+    const target = tab.dataset.tab;
+
+    document.getElementById(target).classList.add("active");
+  });
+});
